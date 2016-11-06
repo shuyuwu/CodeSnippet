134c59a9f7737fe472c91fea7775386dcc454960
ShuyuWu <swu.tamu@gmail.com>
2016-11-05 21:34:42 -0700

    Add Graph DFS implementation

---
+++ GraphDfs.java
@@ -0,0 +1,30 @@
+public class GraphDepthFirstTraversal
+{
+    public void traverseEntireGraph(HashMap<String, List<String>> adjList)
+    {
+        HashSet<String> visited = new HashSet<>();
+
+        for (String node : adjList.keySet())
+        {
+            dfs(adjList, visited, node);
+        }
+    }
+
+    public void dfs(
+        HashMap<String, List<String>> adjList,
+        HashSet<String> visited,
+        String currNode)
+    {
+        if (visited.contains(currNode))
+        {
+            return;
+        }
+
+        visited.add(currNode);
+
+        for (String neighborNode : adjList.get(currNode))
+        {
+            dfs(adjList, visited, neighborNode);
+        }
+    }
+}
