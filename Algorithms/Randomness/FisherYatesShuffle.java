d9b1bf3dc64e3b406978ea37c78e1b4c50e96985
ShuyuWu <swu.tamu@gmail.com>
2016-12-02 22:08:29 -0800

    Add implementation for Fisher-Yates Shuffle

---
+++ Algorithms/Randomness/FisherYatesShuffle.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class FisherYatesShuffle
+{
+    public void shuffle(int[] nums)
+    {
+        if (nums == null || nums.length == 0) return;
+
+        Random r = new Random();
+
+        for (int i = nums.length-1; i >= 0; i--)
+        {
+            int j = r.nextInt(i+1);
+
+            int t = nums[j];
+            nums[j] = nums[i];
+            nums[i] = t;
+        }
+    }
+}
