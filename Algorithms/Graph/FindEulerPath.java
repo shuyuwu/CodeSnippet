00af2300756445f289f6f225d7a3592abf335b64
ShuyuWu <swu.tamu@gmail.com>
2017-07-12 23:36:13 -0700

    Add implementation of Finding Euler Path

---
+++ Algorithms/Graph/FindEulerPath.java
@@ -0,0 +1,28 @@
+package io.nnyy.algorithms.graph;
+
+public class FindEulerPath
+{
+    public void findEulerPath(HashMap<String, Queue<String>> adjList)
+    {
+        if (adjList == null) return;
+
+        LinkedList<String> ll = new LinkedList<>();
+        for (String node : adjList.keySet()) dfs(adjList, ll, node);
+    }
+
+    private void dfs(
+            HashMap<String, Queue<String>> adjList,
+            LinkedList<String> ll,
+            String currNode)
+    {
+        Queue<String> neighbors = adjList.get(currNode);
+
+        while (neighbors.size() > 0)
+        {
+            String neighbor = neighbors.poll();
+            dfs(adjList, ll, neighbor);
+        }
+
+        ll.addFirst(currNode);
+    }
+}
